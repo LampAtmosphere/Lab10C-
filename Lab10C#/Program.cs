9beb4ebb7ede1eb35d9f840b9edb972c60bec673
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2024-04-29 10:00:13 +0300

    Добавьте файлы проекта.

---
+++ Lab10C#/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class FirstClass
+{
+
+    public event Action<string> MyEvent;
+
+    public FirstClass(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+    public void GenerateEvent()
+    {
+        MyEvent?.Invoke(Name);
+    }
+}
+
+
+public class SecondClass
+{
+    public void HandleEvent(string name)
+    {
+        Console.WriteLine($"Событие сгенерировано объектом: {name}");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        FirstClass obj1 = new("Объект 1");
+        FirstClass obj2 = new("Объект 2");
+
+        SecondClass objHandler = new SecondClass();
+
+        obj1.MyEvent += objHandler.HandleEvent;
+        obj2.MyEvent += objHandler.HandleEvent;
+
+        obj1.GenerateEvent();
+        obj2.GenerateEvent();
+    }
+}
